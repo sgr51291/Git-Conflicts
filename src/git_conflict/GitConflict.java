87639c74d37753efab1bd5a2135b8857681f73be
Sagar Yadav <sgr.51291@gmail.com>
2024-02-24 19:13:45 +0530

    Branch A Changes

---
+++ src/git_conflict/GitConflict.java
@@ -0,0 +1,11 @@
+package git_conflict;
+
+public class GitConflict {
+	public static void main(String[] args) {
+		System.out.println("Project Upload in branch A and B");
+		System.out.println("Project A");
+		System.out.println("Project B");
+		System.out.println("-------------------");
+		System.out.println("On Branch A for Sagar Yadav");
+	}
+}
