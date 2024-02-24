d8810c656d6f498066d8bf2112e30302b76dae7a
Sagar Yadav <sgr.51291@gmail.com>
2024-02-24 20:23:06 +0530

    Branch B

--- src/git_conflict/GitConflict.java
+++ src/git_conflict/GitConflict.java
@@ -3,10 +3,10 @@ package git_conflict;
 public class GitConflict {
 	public static void main(String[] args) {
 		System.out.println("Project Upload in branch A and B");
-		System.out.println("Project A");
 		System.out.println("Project B");
-		System.out.println("-------------------");
+		System.out.println("Project A");
 		System.out.println("----------On Branch A for Sagar Yadav----------");
-	
+		System.out.println("On Branch B for Sagar Yadav");
+		System.out.println("Line 10");
 	}
 }
