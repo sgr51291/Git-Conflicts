849024f8d27a9c416410a2d7c29699e2c423833e
Sagar Yadav <sgr.51291@gmail.com>
2024-02-24 19:48:29 +0530

    Branch A

--- src/git_conflict/GitConflict.java
+++ src/git_conflict/GitConflict.java
@@ -6,9 +6,7 @@ public class GitConflict {
 		System.out.println("Project A");
 		System.out.println("Project B");
 		System.out.println("-------------------");
-		System.out.println("----------On Branch B for Sagar Yadav----------");
-		System.out.println("On line 10 branch B");
-		System.out.println("On line 11 branch B");
-		System.out.println("On line 12 branch B");
+		System.out.println("----------On Branch A for Sagar Yadav----------");
+	
 	}
 }
